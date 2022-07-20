66ff0aff398cf1c9dab2d2ffdd21db74d5e334e5
Grishka <ignacioencizo@gmail.com>
2022-07-20 17:41:52 -0300

    Initial commit

---
+++ src/main/java/java/globallogic/evaluate/msgllogin/MsglLoginApplication.java
@@ -0,0 +1,13 @@
+package java.globallogic.evaluate.msgllogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsglLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsglLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/globallogic/evaluate/msgllogin/MsglLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package java.globallogic.evaluate.msgllogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsglLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
